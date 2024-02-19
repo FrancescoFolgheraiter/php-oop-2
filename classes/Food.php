0823525e8fe6f3d511d75477f1d580c1a40a8c18
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-02-19 16:02:51 +0100

    add property food

--- classes/Food.php
+++ classes/Food.php
@@ -1,6 +1,15 @@
 <?php
 require_once __DIR__.'/Products.php';
-class Product{
-    
+class Food extends Product{
+    public $expire;
+    public $flavor;
+    public $quantity;
+
+    function __construct(string $name, int $price, string $category, int $stock = 0, string $description = "",string $image = "immagine non trovata", int $raiting = 0, int $quantity = 0, string $expire ="00/00/0000", string $flavor = ""){
+        parent::__construct( $name,  $price,  $category,  $stock ,  $description , $image ,  $raiting );
+        $this->expire = $expire;
+        $this->flavor= $flavor;
+        $this->quantity = $quantity;
+    }
 }
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
+require_once __DIR__.'/classes/Products.php';
+require_once __DIR__.'/classes/Food.php';
 
-
+$pippo = new Food("prova",5,"dog");
+var_dump($pippo);
 ?>
 <!DOCTYPE html>
 <html lang="en">
