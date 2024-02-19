143420ee3a0916f928425c73d236d7fb4ee05221
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-02-19 15:41:01 +0100

    add property products

--- classes/Products.php
+++ classes/Products.php
@@ -1,5 +1,21 @@
 <?php
 class Product{
-    
+   public $name;
+   public $image;
+   public $price;
+   public $category;
+   public $stock;
+   public $description;
+   public $raiting;
+
+   function __construct(string $name, int $price, string $category, int $stock = 0, string $description = "",string $image = "immagine non trovata", int $raiting = 0){
+    $this->name = $name;
+    $this->image = $image;
+    $this->price = $price;
+    $this->category = $category;
+    $this->stock = $stock;
+    $this->description = $description;
+    $this->raiting = $raiting ;
+   }
 }
 ?>
