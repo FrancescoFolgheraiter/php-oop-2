7c23c7121f4d30a39a2ae27fa894775e13490fdd
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-02-19 16:09:14 +0100

    add property toys

--- classes/Toys.php
+++ classes/Toys.php
@@ -1,6 +1,13 @@
 <?php
 require_once __DIR__.'/Products.php';
-class Product{
-    
+class Toys extends Product{
+    public $material;
+    public $outdoor;
+
+    function __construct(string $name, int $price, string $category, int $stock = 0, string $description = "",string $image = "immagine non trovata", int $raiting = 0, string $material ="unknow", bool $outdoor = false){
+        parent::__construct( $name,  $price,  $category,  $stock ,  $description , $image ,  $raiting );
+        $this->material = $material;
+        $this->outdoor= $outdoor;
+    }
 }
 ?>
--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 require_once __DIR__.'/classes/Products.php';
 require_once __DIR__.'/classes/Food.php';
+require_once __DIR__.'/classes/Toys.php';
 
 $pippo = new Food("prova",5,"dog");
 var_dump($pippo);
+
+$pluto = new Toys("Mangia gatti",30,"dog",10,"dsajidsajdsai","http://prova.com",5,"salciccia",true);
+var_dump($pluto);
 ?>
 <!DOCTYPE html>
 <html lang="en">
