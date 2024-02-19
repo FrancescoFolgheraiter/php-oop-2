f5a3beec9ffb64b6c2eb0fc3fb337c8d2c4a9a8b
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-02-19 16:23:55 +0100

    add property DogBed

--- classes/DogBed.php
+++ classes/DogBed.php
@@ -1,6 +1,13 @@
 <?php
 require_once __DIR__.'/Products.php';
-class Product{
-    
+class DogBed extends Product{
+    public $material;
+    public $size;
+
+    function __construct(string $name, int $price, string $category, int $stock = 0, string $description = "",string $image = "immagine non trovata", int $raiting = 0, string $material ="unknow", int $size = 0){
+        parent::__construct( $name,  $price,  $category,  $stock ,  $description , $image ,  $raiting );
+        $this->material = $material;
+        $this->size= $size;
+    }
 }
 ?>
--- index.php
+++ index.php
@@ -2,12 +2,22 @@
 require_once __DIR__.'/classes/Products.php';
 require_once __DIR__.'/classes/Food.php';
 require_once __DIR__.'/classes/Toys.php';
+require_once __DIR__.'/classes/DogBed.php';
+
+$allProduct = [];
 
 $pippo = new Food("prova",5,"dog");
 var_dump($pippo);
-
+$allProduct[] = $pippo;
+var_dump($allProduct);
 $pluto = new Toys("Mangia gatti",30,"dog",10,"dsajidsajdsai","http://prova.com",5,"salciccia",true);
 var_dump($pluto);
+$allProduct[] = $pluto;
+var_dump($allProduct);
+$paperino= new DogBed("",30,"",10,"","",5,"",30);
+var_dump($paperino);
+$allProduct[] = $paperino;
+var_dump($allProduct);
 ?>
 <!DOCTYPE html>
 <html lang="en">
